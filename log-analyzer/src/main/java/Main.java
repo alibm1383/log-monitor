b6bbe096d1ed144eabfb3f20cc2ef89f56ad7897
Ali Bakhtiari Moghaddam <bakhtiarimoghadam1383@gmail.com>
2026-07-18 19:00:21 +0330

    add json and also connect to kafka

--- log-analyzer/src/main/java/Main.java
+++ log-analyzer/src/main/java/Main.java
@@ -1,6 +1,16 @@
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Properties;
+
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 
 public class Main {
@@ -23,6 +33,20 @@ public class Main {
             return;
         }
 
+        String component = extractComponent(filePath);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        String topic = "raw-logs";
+
         try (BufferedReader reader = new BufferedReader(input)) {
 
             String line;
@@ -36,7 +60,6 @@ public class Main {
 
 
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
                 lines++;
                 try
                 {
@@ -46,23 +69,31 @@ public class Main {
                     String date = details[0];
                     String time = details[1];
                     dateTime = LocalDateTime.parse(date+" "+time,formatter);
-                    thread = details[2];
+                    thread = details[2].substring(1,details[2].length()-1);
                     level = details[3];
                     logger = details[4];
-                    String component = extractComponent(filePath);
                     logs++;
                     LogEntry log = new LogEntry(component,dateTime,thread,level,logger,message);
-
+                    String json = mapper.writeValueAsString(log);
+                    ProducerRecord<String, String> record = new ProducerRecord<>(topic, component, json);
+                    producer.send(record).get();
+                    System.out.println("JSON: " + json);
 
                 }
                 catch (Exception e)
                 {
                     malformed++;
+                    System.out.println("Error on line:");
+                    System.out.println(line);
+                    e.printStackTrace();
                 }
             }
 
         } catch (IOException e) {
             System.out.println("Error reading file: " + e.getMessage());
+        }
+        finally {
+            producer.close();
         }
     }
 
