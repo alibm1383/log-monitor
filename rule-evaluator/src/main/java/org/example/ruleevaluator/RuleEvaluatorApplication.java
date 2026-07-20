77294dbec2d0bb9e4ee38b045cb5e4a5c378b70c
Ali Bakhtiari Moghaddam <bakhtiarimoghadam1383@gmail.com>
2026-07-20 22:38:50 +0330

    Initialize rule-evaluator project

---
+++ rule-evaluator/src/main/java/org/example/ruleevaluator/RuleEvaluatorApplication.java
@@ -0,0 +1,13 @@
+package org.example.ruleevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleEvaluatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuleEvaluatorApplication.class, args);
+    }
+
+}
---
+++ rule-evaluator/src/test/java/org/example/ruleevaluator/RuleEvaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ruleevaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEvaluatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
